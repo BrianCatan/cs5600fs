08ad5c732bf2b84e941c2cf8aa78c7a262c1aa1c
Brian Catanzaro <briancatan@gmail.com>
2015-10-17 18:11:26 -0500

    Added base functionality for server. (NOT TESTED -- GET NOT IMPLEMENTED)

---
+++ server.rb
@@ -0,0 +1,118 @@
+require 'socket'
+
+# Open server on port 8686
+server = TCPServer.open 8686
+
+# Continually listen for incoming connection requests
+loop do
+  # On each request spawn a listener thread
+  Thread.start(server.accept) do |client|
+    command_phrase = client.gets.chomp.chomp.to_sym
+    command_phrase[0] = ''
+    command = command_phrase.split
+
+    # Parse command
+    case command[0]
+
+    when 'createtracker'
+      # createtracker filename filesize description md5 ip-address port-number
+      begin
+        if File.exist? '#{command[1]}.track'
+          # Disallow duplicate trackers
+          client.puts '<createtracker ferr>'
+        else
+          # Create the new tracker file
+          new_tracker = File.new '#{command[1]}.track', 'w'
+          new_tracker.puts 'Filename: #{command[1]}'
+          new_tracker.puts 'Filesize: #{command[2]}'
+          new_tracker.puts 'Description: #{command[3]}'
+          new_tracker.puts 'MD5: #{command[4]}'
+          new_tracker.puts ''
+          new_tracker.puts '# Begin seeding peers'
+          new_tracker.puts '#{command[5]}:#{command[6]}:0:#{command[2]}:#{Time.new(::now).to_i}'
+          new_tracker.close
+
+          client.puts '<createtracker succ>'
+        end
+      rescue
+        # On error remove file to prevent malformed trackers
+        if File.exist? '#{command[1]}.track'
+          File.delete '#{command[1]}.track'
+        end
+        client.puts '<createtracker fail>'
+      end
+
+    when 'updatetracker'
+      # updatetracker filename start_bytes end_bytes ip-address port-number
+      begin
+        if !File.exist? '#{command[1]}.track'
+          # Can't update nonexistant trackers
+          client.puts '<updatetracker #{command[1]} ferr>'
+        else
+          # Copy contents of old tracker to new one
+          File.open('#{command[1]}.track.tmp', 'w') do |update_tracker|
+            File.foreach('#{command[1]}.track') do |line|
+              if line.split(':')[0] != command[4]
+                # Write all lines save those targeting updated IP
+                update_tracker.puts line
+              end
+            end
+            # Append new information to tracker file and delete old tracker
+            update_tracker.puts '#{command[4]}:#{command[5]}:#{command[2]}:#{command[3]}:#{Time.new(::now).to_i}'
+            File.delete '#{command[1]}.track'
+          end
+          # Rename new tracker
+          File.rename '#command{1}.track.tmp', '#{command{1}.track'
+          client.puts '<updatetracker #{command[1]} succ>'
+        end
+      rescue
+        # On error remove any tmp files to prevent malformed trackers
+        if File.exist? '#{command[1]}.track.tmp'
+          File.delete '#{command[1]}.track.temp'
+        end
+        client.puts '<updatetracker #{command[1]} fail>'
+      end
+
+    when 'REQ'
+      # REQ LIST
+      if command[1] != 'LIST'
+        client.puts 'Improper command -- ' + command_phrase
+      end
+      
+      files = Dir.entries '.'
+      files.each 
+      { |f_name|
+        if f_name.split('.')[-1] != 'track'
+          files.delete 'f_name'
+        end
+      }
+      client.puts '<REP LIST #{files.length}>'
+      file_num = 1
+      files.each
+      { |f_name|
+        size = ''
+        md5 = ''
+        File.foreach('#{f_name}.track') do |line|
+          if line.split(:)[0] == 'Filesize'
+            size = line.split(:)[1]
+            size[0] = ''
+          else if line.split(:[0] == 'MD5'
+            md5 = line.split(:)[1]
+            md5[0] = ''
+          end
+          
+          break if md5 != '' and size != ''
+        end
+        client.puts '<#{file_num} #{f_name} #{size} #{md5}>'
+        file_num += 1
+      }
+      client.puts '<REP LIST END>'
+
+    when 'GET'
+      # GET filename.track
+      
+
+    else client.puts 'Improper command -- ' + command_phrase
+    end
+  end
+end
